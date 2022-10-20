cc52253d0a2107675e17b929c1fde92d5731fd36
Fatih Aytuğ Gör <fatihaytuggor@gmail.com>
2022-10-20 18:55:43 +0300

    some updates

--- server/index.js
+++ server/index.js
@@ -5,6 +5,17 @@ const app= express();
 require("dotenv").config();
 app.use(cors());
 app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("DB connection established");
+}).catch((err)=>{
+    console.log(err.message);
+});
+
+
 const server = app.listen(process.env.PORT, ()=>{
     console.log('Server Started on Port ${process.env.PORT}');
 });
